f9b545081dc14d9ca66970075bcc423fdb7afec4
Yrmee <80338690+Yrmee@users.noreply.github.com>
2021-05-07 14:09:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Hallo Winc Academy studenten")
+
+/*
+// "Dit is een grote som"
+let sum = 1230941 * 1823794;
+console.log(sum);
+
+let sum2 = 637263 / 54;
+console.log(sum2);
+*/
+
+// Je kan de code niet zien omdat ze in een block-comment staan.
+
+/* 
+Deze opdracht lukte niet via een Terminal. Daar moet ik nog even naar 
+waarom dit niet lukt. Ik heb deze opdracht gemaakt met de Intergrated Terminal van VSCode.
+*/
